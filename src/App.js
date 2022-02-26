e7960e3363e0806938453a96fa0d8406e1762496
sulexx1 <sulexxmedia@gmail.com>
2022-02-26 01:08:08 -0800

    clone-fresh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,365 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from "./images/logo1.png";
+import baby from './images/babe.png';
+import theboy from './images/image6.png';
+import eye from './images/logo21.png';
+import dollars from './images/logo20.png';
+import coin from './images/logo22.png';
+import time from './images/logo23.png';
+import p from './images/logo4.png';
+import b from './images/logo5.png';
+import br from './images/logo6.png';
+import t from './images/images.png';
+import k from './images/logo7.png';
+import v from './images/logo8.png';
+import d from './images/logo9.png';
+import w from './images/logo10.png';
+import fiverr from './images/images1.jpg';
+import ex from './images/logo11.png';
+import sc from './images/logo12.png';
+import mu from './images/logo13.png';
+import bl from './images/logo14.png';
+import tr from './images/logo15.png';
+import en from './images/logo16.png';
+import it from './images/logo17.png';
+import de from './images/logo18.png';
+import  ka from './images/logo19.png';
+import mrW from './images/watersonQ.jpg';
+import man from './images/images2.jpg';
+import them from './images/images5.png';
+import lady from './images/images4.jpg';
+import twop from './images/images3.jpg';
+import fresh from './images/fresh.jpeg';
+import facebook from './images/facebook2.png';
+import twitter from './images/twitter2.png';
+import youtube from './images/youtube.png';
+import linkdin from './images/linkedin.png';
+import door from './images/glassdoor.png';
+
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+      <div className='header'>
+        <div className='headerRap'>
+        <div><img src={logo} className="logo"/></div>
+        <div className='list'>
+          <a href='#'>Products</a>
+          <a href='#'>Platform</a>
+          <a href='#'>Company</a>
+          <a href='#'>Resources</a>
+          <a href='#'>Customers</a>
+          <a href='#'>Support</a>  
+        </div>
+        </div>
+      </div>
+      <div className='navs'>
+        <div className='text'>
+          <div className='textbase'>
+           <h1 className='delight'>Delight made <br/>easy</h1>
+            <p className='pman'>We make it fast and easy for your business to delight<br/> customer and employees.</p>
+            <div className='but'>
+            <button className='btn1'>FREE TRAILS</button>
+            <button className='btn2'>CONTACT SALES</button>
+            </div>
+            </div>
+           <div>
+             <img src={baby} className='baby'/>
+           </div>
+        </div>
+      </div>
     </div>
+    <div className='container2'>
+      <div className='constWrap'>
+        <div className='glass'>
+          <img src={theboy} className='theboy'/>
+        </div>
+      <div className='business'>
+        <div className='software'>
+          <h3 className='theH'>Try the bisiness software used by 
+          over 50,000 companies across the 
+          globe to exceed customerand employee expectations.</h3>
+          <div className='bot1'>
+            <button className='cubot'>Customers Service</button>
+            <button className='itbot'>IT Service <br/>Management</button>
+          </div>
+          <div className='bot2'>
+            <button className='hrbot'>HR Management</button>
+            <button className='sabot'>Sales Automation</button>
+          </div>
+          <div className='bot3'>
+            <button className='mabot'>Marketing Automation</button>
+            <button className='aibot'>AII Products & Trials</button>
+          </div>
+          </div>
+      </div>
+      </div>
+    </div>
+    <div className='frictionless'>
+      <div className='frictionH'>
+        <div className='fricWrap'>
+          <h4>Frictionless, simple easy</h4>
+          <p className='theP'> Freshworks make it fast and easy for business to delight their Customers and employees<br/>
+            We take a fresh approach to how businesses discover, engage with, and reduce value 
+            from software throughout their journey.
+          </p>
+        </div>
+      </div>
+      <div className='base'>
+        <div className='basewrap'>
+          <div className='dollar'>
+            <div className='get'>
+           <img src={eye} className='eye'/>
+            <h5>Get and running with no barries</h5>
+            <p className='our'>Try or purchase our software directly from our website, and<br/>onboard in a 
+              matter of days, not months.</p>
+              <a href='#'>Start free trails</a>
+              </div>
+              <div className='get'>
+                <img src={dollars} className='money'/>
+              <h5>Choose your pricing plan</h5>
+              <p className='our'>Our pricing plans are designed for modem businessuse use <br/> cases and 
+                affordable for organizations of all sizes. </p>
+                <a href='#'>See pricing</a>
+              </div>
+          </div>
+          <div className='base2'>
+          <div className='dollar2'>
+            <div className='dollar2Wrap'>
+          <div className='me'>
+           <img src={coin} className='coin'/>
+            <h5>Build and customize as you go</h5>
+            <p className='our'>Extend and tailor experiences to meet your unique business
+            needs, with low-code development and over 1,100 custom apps made 
+            available on our marketplace.</p>
+              <a href='#'>Visit marketplace</a>
+              </div>
+          <div className='me'>
+           <img src={time} className='time'/>
+            <h5>Create value, fast</h5>
+            <p className='our'>Accelerate your lteam's productivity and efficiency with modern
+            automation and collaboration tools, to get tangible results in no time.</p>
+              <a href='#'>Learn how</a>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+       </div>
+       </div>
+       <div className='logospart'>
+         <div className='logowrap'>
+           <div className='headerpage'>
+             <div className='headertext'>
+               <h4 className='trusted'>Trusted by 50k+ customers big and small</h4>
+               <p>We are a leading provider of modem SaaS solutions that solve multiple, complex
+                 business problems to companies of all sizes. businesses from more than <br/> 120 countries
+                 around the world use Freshworks products to delight their customersand employees
+                 every day.
+               </p>
+             </div>
+           </div>
+           <div className='logosCollection'>
+             <div className='logolist1'>
+             <div className='logolist1a'><img src={p}/></div>
+             <div className='logolist1b'><img src={b}/></div>
+             <div className='logolist1c'><img src={br}/></div>
+             <div className='logolist1d'><img src={t}/></div>
+             <div className='logolist1e'><img src={k}/></div>
+              </div>
+
+              <div className='logolist2'>
+              <div className='logolist2a'><img src={v}/></div>
+              <div className='logolist2b'><img src={d}/></div>
+              <div className='logolist2c'><img src={w}/></div>
+              <div className='logolist2d'><img src={fiverr}/></div>
+              <div className='logolist2e'><img src={ex}/></div>
+               </div>
+
+               <div className='logolist3'>
+             <div className='logolist3a'><img src={sc}/></div>
+             <div className='logolist3b'><img src={mu}/></div>
+             <div className='logolist3c'><img src={bl}/></div>
+             <div className='logolist3d'><img src={tr}/></div>
+             <div className='logolist3e'> <img src={en}/></div>
+               </div>
+
+               <div className='logolist4'>
+               <div className='logolist4a'><img src={it}/></div>
+               <div className='logolist4b'><img src={de}/></div>
+               <div className='logolist4c'><img src={ka}/></div>
+             </div>
+             </div>
+           </div>
+         </div>
+
+         <div className='theTeam'>
+           <div className='teamHold'>
+             <div className='myhead'>
+               <div className='myheadIMG'><img src={mrW}/></div>
+             </div>
+             <div className='mybody'>
+               <p className='mybodypara'>"The freshworks team was a great asset during implementation and countinue to be a key 
+                 partner for <br/>waterstons as we look to further mature and improve our services to customers,
+                 tailored to their <br/>specific needs".
+               </p>
+               <h4 className='mybodyH'>Trusted by 50k+ customers big and small</h4>
+             </div> 
+             </div>
+         </div>
+
+         <div className='thePeople'>
+           <div className='people'>
+             <div className='docHead'>
+               <div><h3>We aspire to be one of the most loved companies in the world</h3></div>
+               </div>
+             <div className='peoples'>
+               <div className='people1'>
+                 <div className='oga'><img src={man}/></div>
+                 <div className='culture'>
+                   <h5>Culture</h5>
+                   <p>A culture that Supports high-quality work, joy and pride in that work,
+                     speed to execution and intense customer focus.
+                   </p>
+                 </div>
+               </div>
+
+               <div>
+               <div className='people1'>
+                 <div className='oga'><img src={them}/></div>
+                 <div className='culture'>
+                   <h5>Diversity</h5>
+                   <p>Full-spectrum diversity, <br/>equity, and inclusion are key properties for us.
+                   </p>
+                 </div>
+               </div>
+               </div>
+
+               <div>
+               <div className='people1'>
+                 <div className='oga'><img src={twop}/></div>
+                 <div className='culture'>
+                   <h5>Experience</h5>
+                   <p>Focus on enhancing the team experience by strengthening our managers' leadership
+                     capabilities.
+                   </p>
+                 </div>
+               </div>
+               </div>
+
+               <div>
+               <div className='people1'>
+                 <div className='oga'><img src={lady}/></div>
+                 <div className='culture'>
+                   <h5>Team</h5>
+                   <p>4,000 employees and growing in offices across India, US, Europe, and Australia.
+                   </p>
+                 </div>
+               </div>
+               </div>
+             </div>
+             <div className='buttonP'><button className='theButton'>SEE LIFE AT FRESHWORKS</button></div>
+           </div>
+         </div>
+
+         <div className='mrFresh'>
+           <div className='freshP'>
+             <h2>Ready to get started?</h2>
+             <p>Join our community of 50,000+ companiesof all sizes who use Freshworks' modem 
+               SaaS products to make it fast and <br/> easy to delight their customersand employees
+             </p>
+             <button className='contactB'>CONTACT SALES</button>
+           </div>
+         </div>
+
+         <div className='theList'>
+           <div className='mrThelist'>
+             <div className='listThem'>
+               <div>
+                 <img src={logo} className='onpoint'/>
+                 <div className='mrFresh1'>
+                 <li>Freshdesk</li>
+                 <li>Freshsales</li>
+                 <li>Freshmarketer</li>
+                 <li>Freshservice</li>
+                 <li>Freshteam</li>
+                 </div>
+               </div>
+               
+               <div className='mycompany'>
+                 <h5 className='company'> COMPANY </h5> 
+                  <li>About    </li>
+                  <li>Board of Directors</li>
+                  <li>Investors</li>
+                  <li>Customers</li>
+                  <li>Affiliates</li>
+                  <li>Partners</li>
+                  <li>Philanthropy</li>
+                  <li>Careers</li>
+                  <li>News room</li>
+                  <li>GDPR</li>
+                  <li>Tax FAQs</li>
+                  <li>Contact us</li>              
+               </div>
+               
+               <div className='contactC'>
+                 <div>
+                 <h5 className='connect'>CONNECT WITH US</h5>
+                 </div>
+                 <div className='connect1'>
+                   <img src={facebook}/>
+                   <img src={twitter}/>
+                   <img src={youtube}/>
+                 </div>
+                 <div className='connect2'>
+                   <img src={linkdin}/>
+                   <img src={door}/>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         
+         <div className='mrLast'>
+           <div className='goodluck'>
+      <div className="Terms">
+        Terms of service - Privacy Notice - Takedown Policy - GDPR - Security - CS Policy - Tax FAQs - Unsubscribe</div>
+
+
+      <div className="copyright">Copyright (c) Freshworks Inc. All Rights Reserved</div>
+    </div>
+    </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+//const [add, setAdd] = useState(0);
+  
+  //function plus (){
+    //setAdd(add +1)
+  //}
+  //const minus= () =>{
+    //setAdd(add -1)
+//}
+
+//<div className='mainContainer'>
+  //    <div className='Holder'>
+    //  <button onClick={minus}>-</button>
+      //<h2>{add}</h2>
+      //<button onClick={plus}>+</button>
+      //</div>
+    //</div>
